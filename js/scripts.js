c1c84ea34a52428b15d4530df507e3e8baa02742
wageres <61704573+wageres@users.noreply.github.com>
2021-09-29 16:26:48 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
-/* Когда пользователь нажимает на кнопку,
-переключение между скрытием и отображением раскрывающегося содержимого */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Закройте выпадающее меню, если пользователь щелкает за его пределами
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
